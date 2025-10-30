e3ab514bbdad06d1f952e79f17e4e3043a97f03c
Divyansh Gupta <gdivyansh442@gmail.com>
2025-10-30 22:33:46 +0530

    logo changed

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bottom-0 text-center p-4 text-sm text-gray-500'>
+      <h3>Developed by Divyansh Gupta</h3>
+    </footer>
+  )
+}
+
+export default Footer
